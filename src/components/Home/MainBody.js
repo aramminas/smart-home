37314e012bc2d42444b57ad229f6f65559914156
JoraHarutyunyan <jora.harutyunyan.y@gmail.com>
2019-11-03 18:16:55 +0400

    changed shields(on hover)

--- src/components/Home/MainBody.js
+++ src/components/Home/MainBody.js
@@ -4,6 +4,7 @@ import icon1 from './Design_MainPage/icon1.png';
 import icon2 from './Design_MainPage/icon2.png';
 import icon3 from './Design_MainPage/icon3.png';
 import icon4 from './Design_MainPage/icon4.png';
+import empty from './Design_MainPage/icon_empty.png';
 
 function MainBody () {
 
@@ -13,9 +14,17 @@ function MainBody () {
                 <Row>
                     <Col lg={{size:6}} md={{size:6}} sm={{size:6}} xs={{size:6}} className="shield shieldsLeft" id="shield1">
                         <img id="img1" src={icon1} alt="icon1"/>
+                        <div id="img1Empty">
+                            <img id="image1Empty" src={empty} alt="icon_empty"/>
+                            <h1 class = "des">WIFI Connection</h1>
+                        </div>
                     </Col>
                     <Col lg={{size:6}} md={{size:6}} sm={{size:6}} xs={{size:6}} className="shield shieldsLeft" id="shield2">
                         <img id="img2" src={icon2} alt="icon2"/>
+                        <div id="img2Empty">
+                            <img id="image2Empty" src={empty} alt="icon_empty"/>
+                            <h1 class = "des">Access Control</h1>
+                        </div>
                     </Col>
                 </Row>
             </Col>
@@ -23,9 +32,17 @@ function MainBody () {
                 <Row>
                     <Col lg={{size:6}} md={{size:6}} sm={{size:6}} xs={{size:6}} className="shield shieldsRight" id="shield3">
                         <img id="img3" src={icon3} alt="icon3"/>
+                        <div id="img3Empty">
+                            <img id="image3Empty" src={empty} alt="icon_empty"/>
+                            <h1 class = "des">No Phone Line Needed</h1>
+                        </div>
                     </Col>
                     <Col lg={{size:6}} md={{size:6}} sm={{size:6}} xs={{size:6}} className="shield shieldsRight" id="shield4">
                         <img id="img4" src={icon4} alt="icon4"/>
+                        <div id="img4Empty">
+                            <img id="image4Empty" src={empty} alt="icon_empty"/>
+                            <h1 class = "des">24/7 monitoring</h1>
+                        </div>
                     </Col>
                 </Row>
             </Col>
